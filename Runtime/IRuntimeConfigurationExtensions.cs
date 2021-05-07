9c029634c795da0c0fa57d999ef3fdde74c3c997
Marcello Tridenti <marcello.tridenti@tutanota.com>
2021-05-07 16:17:19 +0200

    Removed mentions of creator

--- Runtime/IRuntimeConfigurationExtensions.cs
+++ Runtime/IRuntimeConfigurationExtensions.cs
@@ -17,7 +17,7 @@ namespace VPG.Creator.TextToSpeech
         /// <summary>
         /// Return loaded <see cref="TextToSpeechConfiguration"/>.
         /// </summary>
-        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
+        [Obsolete("To be more flexible with development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
         public static TextToSpeechConfiguration GetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration)
         {
             if (textToSpeechConfiguration == null)
@@ -31,7 +31,7 @@ namespace VPG.Creator.TextToSpeech
         /// <summary>
         /// Loads a new <see cref="TextToSpeechConfiguration"/>
         /// </summary>
-        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
+        [Obsolete("To be more flexible with development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
         public static void SetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration, TextToSpeechConfiguration ttsConfiguration)
         {
             textToSpeechConfiguration = ttsConfiguration;
